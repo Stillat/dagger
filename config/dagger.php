9c1970e7f41ecd58c6641169d4365c641d166ca6
John Koster <john@stillat.com>
2025-01-26 19:15:10 -0600

    Few more.

--- config/dagger.php
+++ config/dagger.php
@@ -5,6 +5,15 @@ return [
         'unsafe_functions' => [
             // Date/Time related.
             'now', 'time', 'date',
+
+            // Environment related.
+            'env', 'getenv',
+
+            // Request related.
+            'cookie', 'request', 'session',
+
+            // Debugging related.
+            'dd', 'dump', 'var_dump', 'debug_backtrace', 'phpinfo',
         ],
     ],
 ];
